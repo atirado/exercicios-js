614d15a002dae46ec4648d7603ec081b0ef17246
FHELIPE MORAES <04020745056@senacrs.edu.br>
2024-03-05 11:30:30 -0300

    exercicios js

---
+++ index.js
@@ -0,0 +1,19 @@
+    // comando para exibir mensagem 
+    console.log("O INTER VAI SAIR CAMPEÃO E O GREMIO NÃO!!")
+
+    // exercicio de calculos
+
+    const num1 = 20;
+    const num2 = 654;
+
+    // soma
+    let soma = num1 + num2;
+    let subtração = num1 - num2;
+    let multiplicação = num1 * num2;
+    let divisão = num1 / num2;
+    console.log("o valor da soma é " + soma + " - daleeeeeeeeeeee inter");
+    console.log("o valor da soma é " + subtração + " - daleeeeeeeeeeee inter");
+    console.log("o valor da soma é " + multiplicação + " - daleeeeeeeeeeee inter");
+    console.log("o valor da soma é " + divisão + " - daleeeeeeeeeeee inter");
+
+    
